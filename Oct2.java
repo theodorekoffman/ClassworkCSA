25ac4a71005bbf6aff6a050850b20f23ebb29c51
theodorekoffman <55504321+theodorekoffman@users.noreply.github.com>
2019-10-02 09:23:41 -0700

    Add files via upload
    String Processor 2

---
+++ Oct2.java
@@ -0,0 +1,35 @@
+
+
+/**
+ * Write a description of class Oct2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Oct2
+{
+public static String withoutEnds(String str)
+   {
+     return str.substring(1, str.length()-1);
+    }
+    public static String twoEnd(String str) 
+    {
+    return str.substring(str.length()-2, str.length()) + str.substring(0, str.length() -2);
+}
+  public static String firstFour(String str) 
+  {
+     if (str.length() < 4)
+       {
+       while (i > 4)
+       {
+           //str= str 
+           
+}
+}
+}
+public static void main(String[] args)
+{
+    boolean a = StringPocessor2.FrontAgain("edited");
+    System.out.println(a);
+
+}
